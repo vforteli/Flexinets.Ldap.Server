8711f85c065091b9223ee905773f2689492bb187
Unknown <verner@fortelius.net>
2017-01-16 20:00:10 +0200

    Initial commit

---
+++ LdapServer.cs
@@ -0,0 +1,90 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flexinets.Ldap
+{
+    public class LdapServer
+    {
+        private readonly ILog _log = LogManager.GetLogger(typeof(LdapServer));
+        private readonly TcpListener _server;
+
+        public Boolean Running
+        {
+            get;
+            private set;
+        }
+
+
+        /// <summary>
+        /// Create a new server on endpoint
+        /// </summary>
+        /// <param name="serverEndpoint"></param>
+        /// <param name="dictionary"></param>
+        public LdapServer(IPEndPoint serverEndpoint)
+        {
+            _server = new TcpListener(serverEndpoint);
+        }
+
+
+        /// <summary>
+        /// Start listening for requests
+        /// </summary>
+        public void Start()
+        {
+            _server.Start();
+            _server.BeginAcceptTcpClient(ReceiveCallback, null);
+            Running = true;
+        }
+
+
+        /// <summary>
+        /// Stop listening
+        /// </summary>
+        public void Stop()
+        {
+            _server.Stop();
+            Running = false;
+        }
+
+
+        /// <summary>
+        /// Receive packets
+        /// </summary>
+        /// <param name="ar"></param>
+        private void ReceiveCallback(IAsyncResult ar)
+        {
+            if (Running)
+            {
+                _log.Debug("huurr");
+                var client = _server.EndAcceptTcpClient(ar);
+                _log.Debug("got a connection?");
+                // Immediately start listening for the next packet
+                _server.BeginAcceptTcpClient(ReceiveCallback, null);
+
+                var stream = client.GetStream();
+                var reader = new StreamReader(stream);
+                var writer = new StreamWriter(stream) { AutoFlush = true };
+
+                while (true)
+                {
+                    string inputLine = "";
+                    while (inputLine != null)
+                    {
+                        _log.Debug(inputLine);
+                        inputLine = reader.ReadLine();
+                        writer.WriteLine("Echoing string: " + inputLine);
+                        Console.WriteLine("Echoing string: " + inputLine);
+                    }
+                    Console.WriteLine("Server saw disconnect from client.");
+                }
+            }
+        }
+    }
+}
