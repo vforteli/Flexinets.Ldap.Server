4002108ab7e65db2c90a45668faa0f6c57fb1ef1
Unknown <verner@fortelius.net>
2017-02-06 13:32:03 +0200

    Add function for converting integer to BER encoded length

---
+++ Tests/BerLengthTests.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flexinets.Ldap.Tests
+{
+    [TestClass]
+    public class BerLengthTests
+    {
+        [TestMethod]
+        public void TestBerLengthShortNotation1()
+        {
+            var berlength = Utils.IntToBerLength(1);
+            Assert.AreEqual("10000000", Utils.BitsToString(new BitArray(berlength)));
+        }
+
+
+        [TestMethod]
+        public void TestBerLengthShortNotation2()
+        {
+            var berlength = Utils.IntToBerLength(127);
+            Assert.AreEqual("11111110", Utils.BitsToString(new BitArray(berlength)));
+        }
+
+
+        [TestMethod]
+        public void TestBerLengthLongNotation1()
+        {
+            var berlength = Utils.IntToBerLength(128);
+            Assert.AreEqual("10000001 00000001", Utils.BitsToString(new BitArray(berlength)));
+        }
+
+
+        [TestMethod]
+        public void TestBerLengthLongNotation2()
+        {
+            var berlength = Utils.IntToBerLength(256);
+            Assert.AreEqual("01000001 00000000 10000000", Utils.BitsToString(new BitArray(berlength)));
+        }
+
+
+        [TestMethod]
+        public void TestBerLengthLongNotation3()
+        {
+            var berlength = Utils.IntToBerLength(65536);
+            Assert.AreEqual("11000001 00000000 00000000 10000000", Utils.BitsToString(new BitArray(berlength)));
+        }
+
+
+        [TestMethod]
+        public void TestBerLengthLongNotation4()
+        {
+            var berlength = Utils.IntToBerLength(255);
+            Assert.AreEqual("10000001 11111111", Utils.BitsToString(new BitArray(berlength)));
+        }
+    }
+}
--- Utils.cs
+++ Utils.cs
@@ -48,5 +48,44 @@ namespace Flexinets.Ldap
             }
             return derp.Trim();
         }
+
+
+        /// <summary>
+        /// Convert integer length to a byte array with BER encoding
+        /// https://en.wikipedia.org/wiki/X.690#BER_encoding
+        /// </summary>
+        /// <param name="length"></param>
+        /// <returns></returns>
+        public static Byte[] IntToBerLength(Int32 length)
+        {
+            // Short notation
+            if (length <= 127)
+            {
+                return new byte[] { (byte)length };
+            }
+            // Long notation
+            else
+            {
+                var intbytes = BitConverter.GetBytes(length);
+
+                byte intbyteslength = (byte)intbytes.Length;
+
+                // Get the actual number of bytes needed
+                while (intbyteslength >= 0)
+                {
+                    intbyteslength--;
+                    if (intbytes[intbyteslength - 1] != 0)
+                    {
+                        break;
+                    }
+                }
+
+                var lengthByte = intbyteslength + 128;
+                var berBytes = new byte[1 + intbyteslength];
+                berBytes[0] = (byte)lengthByte;
+                Buffer.BlockCopy(intbytes, 0, berBytes, 1, intbyteslength);
+                return berBytes;
+            }
+        }
     }
 }
