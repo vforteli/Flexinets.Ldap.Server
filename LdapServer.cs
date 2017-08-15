365029778664e40b08d3cd1eefa3517c51667964
Unknown <verner@fortelius.net>
2017-08-15 21:16:01 +0300

    use task async

--- LdapServer.cs
+++ LdapServer.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
+using System.Threading.Tasks;
 
 namespace Flexinets.Ldap
 {
@@ -30,7 +31,7 @@ namespace Flexinets.Ldap
         public void Start()
         {
             _server.Start();
-            _server.BeginAcceptTcpClient(ReceiveCallback, null);
+            var receiveTask = StartAcceptingClientsAsync();
         }
 
 
@@ -44,49 +45,66 @@ namespace Flexinets.Ldap
 
 
         /// <summary>
-        /// Receive packets
+        /// Start the loop used for accepting clients
+        /// </summary>
+        /// <returns></returns>
+        private async Task StartAcceptingClientsAsync()
+        {
+            while (_server.Server.IsBound)
+            {
+                try
+                {
+                    var client = await _server.AcceptTcpClientAsync();
+                    var task = Task.Factory.StartNew(() => HandleClient(client), TaskCreationOptions.LongRunning);
+                }
+                catch (ObjectDisposedException) { } // Thrown when server is stopped while still receiving. This can be safely ignored
+                catch (Exception ex)
+                {
+                    _log.Fatal("Something went wrong accepting client", ex);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Handle clients
         /// </summary>
         /// <param name="ar"></param>
-        private void ReceiveCallback(IAsyncResult ar)
+        private void HandleClient(TcpClient client)
         {
-            if (_server.Server.IsBound)
+            try
             {
-                using (var client = _server.EndAcceptTcpClient(ar))
+                _log.Debug($"Connection from {client.Client.RemoteEndPoint}");
+
+                var isBound = false;
+                var stream = client.GetStream();
+
+                while (LdapPacket.TryParsePacket(stream, out var requestPacket))
                 {
-                    _server.BeginAcceptTcpClient(ReceiveCallback, null);
-                    _log.Debug($"Connection from {client.Client.RemoteEndPoint}");
+                    PrintValue(requestPacket);
 
-                    try
+                    if (requestPacket.ChildAttributes.Any(o => o.LdapOperation == LdapOperation.BindRequest))
                     {
-                        var stream = client.GetStream();
+                        isBound = HandleBindRequest(stream, requestPacket);
+                    }
 
-                        while (LdapPacket.TryParsePacket(stream, out var requestPacket))
+                    if (isBound)    // Only handle other requests if the client is bound, dont allow any anonymous searches
+                    {
+                        if (requestPacket.ChildAttributes.Any(o => o.LdapOperation == LdapOperation.SearchRequest))
                         {
-                            PrintValue(requestPacket);
-
-                            if (requestPacket.ChildAttributes.Any(o => o.LdapOperation == LdapOperation.BindRequest))
-                            {
-                                var responseBytes = HandleBindRequest(requestPacket);
-                                stream.Write(responseBytes, 0, responseBytes.Length);
-                            }
-
-                            if (requestPacket.ChildAttributes.Any(o => o.LdapOperation == LdapOperation.SearchRequest))
-                            {
-                                HandleSearchRequest(stream, requestPacket);
-                            }
+                            HandleSearchRequest(stream, requestPacket);
                         }
-
-                        _log.Debug($"Connection closed to {client.Client.RemoteEndPoint}");
-                    }
-                    catch (IOException ioex)
-                    {
-                        _log.Warn("oops", ioex);
-                    }
-                    catch (Exception ex)
-                    {
-                        _log.Error("Something went wrong", ex);
                     }
                 }
+
+                _log.Debug($"Connection closed to {client.Client.RemoteEndPoint}");
+            }
+            catch (IOException ioex)
+            {
+                _log.Warn("oops", ioex);
+            }
+            catch (Exception ex)
+            {
+                _log.Error("Something went wrong", ex);
             }
         }
 
@@ -101,7 +119,7 @@ namespace Flexinets.Ldap
             var searchRequest = requestPacket.ChildAttributes.SingleOrDefault(o => o.Class == TagClass.Application && o.LdapOperation == LdapOperation.SearchRequest);
             var filter = searchRequest.ChildAttributes[6];
 
-            if (filter.ContextType == (Byte)LdapFilterChoice.equalityMatch && filter.ChildAttributes[0].GetValue<String>() == "sAMAccountName" && filter.ChildAttributes[1].GetValue<String>() == "testuser") // equalityMatch
+            if ((LdapFilterChoice)filter.ContextType == LdapFilterChoice.equalityMatch && filter.ChildAttributes[0].GetValue<String>() == "sAMAccountName" && filter.ChildAttributes[1].GetValue<String>() == "testuser") // equalityMatch
             {
                 var responseEntryPacket = new LdapPacket(requestPacket.MessageId);
                 var searchResultEntry = new LdapAttribute(LdapOperation.SearchResultEntry, true);
@@ -118,8 +136,8 @@ namespace Flexinets.Ldap
             response.ChildAttributes.Add(new LdapAttribute(UniversalDataType.OctetString, false));  // matchedDN
             response.ChildAttributes.Add(new LdapAttribute(UniversalDataType.OctetString, false));  // diagnosticMessage
             responseDonePacket.ChildAttributes.Add(response);
-            var responsDoneBytes = responseDonePacket.GetBytes();
-            stream.Write(responsDoneBytes, 0, responsDoneBytes.Length);
+            var responseDoneBytes = responseDonePacket.GetBytes();
+            stream.Write(responseDoneBytes, 0, responseDoneBytes.Length);
         }
 
 
@@ -127,7 +145,7 @@ namespace Flexinets.Ldap
         /// Handle bindrequests
         /// </summary>
         /// <param name="bindrequest"></param>
-        private Byte[] HandleBindRequest(LdapPacket requestPacket)
+        private Boolean HandleBindRequest(Stream stream, LdapPacket requestPacket)
         {
             var bindrequest = requestPacket.ChildAttributes.SingleOrDefault(o => o.Class == TagClass.Application && o.LdapOperation == LdapOperation.BindRequest);
             var username = bindrequest.ChildAttributes[1].GetValue<String>();
@@ -135,7 +153,7 @@ namespace Flexinets.Ldap
 
             var response = LdapResult.invalidCredentials;
             if (username == "cn=bindUser,cn=Users,dc=dev,dc=company,dc=com" && password == "bindUserPassword"
-                || username == "cn=testuser,cn=Users,dc=dev,dc=company,dc=com" && password == "123")
+                || username == "cn=user,dc=example,dc=com" && password == "123")
             {
                 response = LdapResult.success;
             }
@@ -146,7 +164,9 @@ namespace Flexinets.Ldap
             bindResponse.ChildAttributes.Add(new LdapAttribute(UniversalDataType.OctetString, false));  // matchedDN
             bindResponse.ChildAttributes.Add(new LdapAttribute(UniversalDataType.OctetString, false));  // diagnosticMessage
             responsePacket.ChildAttributes.Add(bindResponse);
-            return responsePacket.GetBytes();
+            var responseBytes = responsePacket.GetBytes();
+            stream.Write(responseBytes, 0, responseBytes.Length);
+            return response == LdapResult.success;
         }
 
 
